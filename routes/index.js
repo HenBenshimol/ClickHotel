187069b40810effb98fe4f07604f73ded0c04dcf
DESKTOP-V75O7I6\Gil <gilgoldberger@gmail.com>
2019-02-21 16:51:34 +0200

    work on General Information

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Click Hotel' });
 });
 
+/* GET General Information page. */
+router.get('/GeneralInformation', function(req, res) {
+  res.render('GeneralInformation', { title: 'General Information', Hotel: 'Hilton' });
+});
+
+
 module.exports = router;
