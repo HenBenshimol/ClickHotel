b4b0787d1050ba8e2081e0389f4ee00c5bf4f8bb
DESKTOP-V75O7I6\Gil <gilgoldberger@gmail.com>
2019-02-22 12:49:08 +0200

    Update hotel.js module

--- models/hotel.js
+++ models/hotel.js
@@ -11,18 +11,47 @@ const Base = mongoose.model('Base', new mongoose.Schema({
         type: String,
         required: true
     },
-    location: {
+    Location: {
         type: String,
         required: true
     },
-    details: {
+    Details: {
         type: String,
         required: true
     }
 }, baseOptions));
 
 //Hotel schema
-const Hotel = Base.discriminator('Hotel', new mongoose.Schema({}));
+const Hotel = Base.discriminator('Hotel', new mongoose.Schema({
+    "Phone number": {
+        type: String,
+        required: true
+    },
+    "Fax number": {
+        type: String,
+        required: true
+    },
+    "Check-in time": {
+        type: String,
+        required: true
+    },
+    "Check-out time": {
+        type: String,
+        required: true
+    },
+    Logo: {
+        type: ObjectId,
+        required: true
+    },
+    Image1: {
+        type: ObjectId,
+        required: true
+    },
+    Image2: {
+        type: ObjectId,
+        required: true
+    },
+}));
 
 //Room schema
 const Room = Base.discriminator('Room', new mongoose.Schema({
